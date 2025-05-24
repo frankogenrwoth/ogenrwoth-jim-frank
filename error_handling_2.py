fa24d77fea5a934f6d1d3641a1d03df9e87a920e
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2025-05-24 07:40:35 +0300

    add assignment two handling errors

---
+++ error_handling_2.py
@@ -0,0 +1,17 @@
+if __name__=="__main__":
+    while True:
+        number_1 = input("number 1: ")
+        number_2 = input("number 2: ")
+
+        try:
+            result = int(number_1) / int(number_2)
+            print("division: ", result)
+            break
+
+        except ValueError as e:
+            print("\n", f"[-] please enter a number not a string", "\n")
+
+        except ZeroDivisionError as e:
+            print("\n", f"[-] Error occured you cannot divide a number by zero", "\n")
+
+    print("\n", "[+] Thank you for using this program", "\n")
