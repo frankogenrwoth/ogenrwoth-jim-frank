3be900d62cfa9486885e6e9f8d884feb3cb57e51
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2025-05-28 09:14:25 +0300

    add polymorphism assignment overloading and overriding

--- polymorphism_assignment.py
+++ polymorphism_assignment.py
@@ -6,15 +6,28 @@ def get_sum(*numbers):
 # overriding
 class A:
     def say_hi(self):
-        pass
+        print("hello from A")
 
 
 class B(A):
     def say_hi(self):
-        print("hello")
+        print("hello from B")
+
+
+# MRO
+class C(A):
+    def say_hi(self):
+        print("hello from C")
+
+
+class D(B, C):
+    pass
 
 
 if __name__ == "__main__":
     print(get_sum(1, 4, 6), get_sum(3, 5))
     b = B()
     b.say_hi()
+
+    d = D()
+    d.say_hi()
