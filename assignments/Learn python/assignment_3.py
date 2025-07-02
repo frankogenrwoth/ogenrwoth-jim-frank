5147c1b1d8c056d4801e90c40c6cd95d67176c05
frankogenrwoth <121643701+frankogenrwoth@users.noreply.github.com>
2025-07-02 17:13:04 +0300

    refactor assignment 3 to learn python

---
+++ assignments/Learn python/assignment_3.py
@@ -0,0 +1,15 @@
+# Assignment 3: factorial of a number: 5
+
+def factorial(n):
+    if n < 0:
+        return ("[-] Factorial of negative numbers is not defined")
+    if n == 1 or n == 0:
+        return 1
+    
+    return n * factorial(n - 1)
+
+
+if __name__ == "__main__":
+    print(factorial(5))
+    print(factorial(0))
+    print(factorial(-2))
